64dc947e70ddec2de865a56b2cfdd62ded3fdee3
Bhavik <sapatbhavik101@gmail.com>
2026-07-08 00:37:55 +0530

    Navbar adjustment fixed

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -11,58 +11,60 @@ const Hero = () => {
   ];
   return (
     <>
-      <div className="flex h-full flex-col justify-center">
-        <div className="items-center mx-30 my-15">
-          <div className="text-white">
-            <p>⭐ Trusted by 1500+ families. </p>
-            <h1 className="font-bold text-6xl my-2 mb-10 text-white ">
-              Find Your Dream Property
-            </h1>
-            <p className="text-2xl mb-7 text-white mix-blend-difference">
-              Discover premium residential and commercial
-              <br />
-              properties with trusted experts.
-            </p>
-            <Link href="#properties">
-              <Button
-                className="py-5 px-5 mr-3 bg-[#6F532D] text-white"
-                variant="secondary"
-              >
-                Explore Properties
-              </Button>
-            </Link>
-            <Link href="#contact">
-              <Button className="py-5 px-5 border border-white bg-white/10 backdrop-blur-xl">
-                Contact Us
-              </Button>
-            </Link>
+      <section id="hero">
+        <div className="flex h-full flex-col justify-center">
+          <div className="items-center mx-30 my-15">
+            <div className="text-white">
+              <p>⭐ Trusted by 1500+ families. </p>
+              <h1 className="font-bold text-6xl my-2 mb-10 text-white ">
+                Find Your Dream Property
+              </h1>
+              <p className="text-2xl mb-7 text-white mix-blend-difference">
+                Discover premium residential and commercial
+                <br />
+                properties with trusted experts.
+              </p>
+              <Link href="#properties">
+                <Button
+                  className="py-5 px-5 mr-3 bg-[#6F532D] text-white"
+                  variant="secondary"
+                >
+                  Explore Properties
+                </Button>
+              </Link>
+              <Link href="#contact">
+                <Button className="py-5 px-5 border border-white bg-white/10 backdrop-blur-xl">
+                  Contact Us
+                </Button>
+              </Link>
+            </div>
+          </div>
+          <div className="flex flex-row justify-center gap-10 w-100vw py-3 bg-[#c2ae9b]/20">
+            {keys.map((item, index) => (
+              <React.Fragment key={index}>
+                <div
+                  className=" py-8 p-6 rounded-xl  text-center transition-all duration-300 hover:shadow-xl"
+
+                  // className="bg-white/20 backdrop-blur-xl py-8 p-6 rounded-xl shadow-xl border  text-center transition-all duration-300 hover:shadow-md"
+                >
+                  <h2 className="text-4xl font-extrabold text-white mb-1">
+                    {item.title}
+                  </h2>
+                  <p className="text-white font-medium tracking-wider text-xs uppercase">
+                    {item.desc}
+                  </p>
+                </div>
+                {index < keys.length - 1 && (
+                  <Separator
+                    orientation="vertical"
+                    className=" md:block h-full w-[1px] bg-white/100"
+                  />
+                )}
+              </React.Fragment>
+            ))}
           </div>
         </div>
-        <div className="flex flex-row justify-center gap-10 w-100vw py-3 bg-[#c2ae9b]/20">
-          {keys.map((item, index) => (
-            <React.Fragment key={index}>
-              <div
-                className=" py-8 p-6 rounded-xl  text-center transition-all duration-300 hover:shadow-xl"
-
-                // className="bg-white/20 backdrop-blur-xl py-8 p-6 rounded-xl shadow-xl border  text-center transition-all duration-300 hover:shadow-md"
-              >
-                <h2 className="text-4xl font-extrabold text-white mb-1">
-                  {item.title}
-                </h2>
-                <p className="text-white font-medium tracking-wider text-xs uppercase">
-                  {item.desc}
-                </p>
-              </div>
-              {index < keys.length - 1 && (
-                <Separator
-                  orientation="vertical"
-                  className=" md:block h-full w-[1px] bg-white/100"
-                />
-              )}
-            </React.Fragment>
-          ))}
-        </div>
-      </div>
+      </section>
     </>
   );
 };
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,41 +10,39 @@ export default function Navbar() {
   ];
   return (
     <>
-      <section id="hero">
-        <nav className="sticky top-0 z-50 px-0">
-          <div className="mx-auto flex w-full items-center justify-between px-6 py-2 bg-[#6F532D]/20 backdrop-blur-sm">
-            <Link
-              href="/"
-              className="flex text-3xl font-bold font-serif text-[#8B6B3E]"
-            >
-              <Image
-                src="/logo.png"
-                className="mr-3"
-                alt="logo"
-                width={35}
-                height={35}
-              />
-              Skyline Realty
-            </Link>
-            <div className="w-auto px-8 py-1.5 rounded-full bg-[#6F532D]/50 backdrop-blur-xl border ">
-              <div className="hidden items-center gap-8 md:flex font-normal uppercase ">
-                {tabs.map((item, index) => (
-                  <Link
-                    key={index}
-                    href={item.link}
-                    className="tracking-wide text-white hover:text-[#8B6B3E]  transition-colors duration-300 scroll-smooth"
-                  >
-                    {item.title}
-                  </Link>
-                ))}
-              </div>
+      <nav className="sticky top-0 z-50 px-0">
+        <div className="mx-auto flex w-full items-center justify-between px-6 py-2 bg-[#6F532D]/20 backdrop-blur-sm">
+          <Link
+            href="/"
+            className="flex text-3xl font-bold font-serif text-[#8B6B3E]"
+          >
+            <Image
+              src="/logo.png"
+              className="mr-3"
+              alt="logo"
+              width={35}
+              height={35}
+            />
+            Skyline Realty
+          </Link>
+          <div className="w-auto px-8 py-1.5 rounded-full bg-[#6F532D]/50 backdrop-blur-xl border ">
+            <div className="hidden items-center gap-8 md:flex font-normal uppercase ">
+              {tabs.map((item, index) => (
+                <Link
+                  key={index}
+                  href={item.link}
+                  className="tracking-wide text-white hover:text-[#8B6B3E]  transition-colors duration-300 scroll-smooth"
+                >
+                  {item.title}
+                </Link>
+              ))}
             </div>
-            <Button className="hidden rounded-lg bg-[#6F532D] px-5 py-4.5 md:inline-flex items-center justify-center  font-medium text-white transition-colors duration-300 hover:bg-[#c2ae9b]  ">
-              Book Consultation
-            </Button>
           </div>
-        </nav>
-      </section>
+          <Button className="hidden rounded-lg bg-[#6F532D] px-5 py-4.5 md:inline-flex items-center justify-center  font-medium text-white transition-colors duration-300 hover:bg-[#c2ae9b]  ">
+            Book Consultation
+          </Button>
+        </div>
+      </nav>
     </>
   );
 }
